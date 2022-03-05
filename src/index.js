6ae0dd17a23c4d5f0e5339e50d8840b1181706c8
Jennifer Adams <wordsword23@gmail.com>
2022-03-05 10:22:14 -0500

    implemented React.StrictMode

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
