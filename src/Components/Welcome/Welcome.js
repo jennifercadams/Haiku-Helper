a53e298fe5d6d7eb0597b660a36b9ccecbdf2f6a
Jennifer Adams <wordsword23@gmail.com>
2022-03-05 08:02:14 -0500

    changed id/className attributes of lines in welcome component to avoid namespace pollution

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -10,9 +10,9 @@ export default class Welcome extends React.Component {
   render() {
     return (
       <div id="welcome">
-        <p className="line-1">I'm Haiku Helper</p>
-        <p className="line-2"><span id="change-text"></span></p>
-        <p className="line-3">For writing haiku</p>
+        <p id="welcome-line-1">I'm Haiku Helper</p>
+        <p id="welcome-line-2"><span id="change-text"></span></p>
+        <p id="welcome-line-3">For writing haiku</p>
         <button id="start" onClick={this.props.start}>Start</button>
       </div>
     )
